f1876d8fe4e9bfd266a6b0d33b93e1dd67fa12b3
telxt <lvxt24@mails.tsinghua.edu.cn>
2026-05-19 13:00:14 +0800

    update scripts

--- scripts/run_teacher_server.sh
+++ scripts/run_teacher_server.sh
@@ -1,5 +1,5 @@
 # For Qwen3-30B-A3B
-python -m sglang.launch_server --model-path path_Qwen3-30B-A3B --tp 2 --ep 2 --dp 4 --port 25431 --host 0.0.0.0 --attention-backend fa3
+python -m sglang.launch_server --model-path path_Qwen3-30B-A3B --tp 2 --ep 2 --dp 4 --port 25431 --host 0.0.0.0 --attention-backend fa3 --chunked-prefill-size 4096
 
 # For GLM-4.7-Flash
-python -m sglang.launch_server --model-path path_GLM-4.7-Flash --tp 2 --ep 2 --dp 4 --port 25431 --host 0.0.0.0 --attention-backend fa3
+python -m sglang.launch_server --model-path path_GLM-4.7-Flash --tp 2 --ep 2 --dp 4 --port 25431 --host 0.0.0.0 --attention-backend fa3 --chunked-prefill-size 4096
